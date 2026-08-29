8fe2ed27e0373f3f54ea0d7a5a6fa350ba344c9d
Eva Emmerich <eva@evaemmerich.com>
2026-08-29 15:35:49 +0200

    init: rush shell.nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  sources = import ./lon.nix;
+  pkgs = import sources.nixpkgs { };
+in
+pkgs.mkShell {
+  packages = [
+    pkgs.nixfmt
+    pkgs.nix-prefetch-git
+    pkgs.clippy
+    pkgs.rustfmt
+    pkgs.rust-analyzer
+    pkgs.leptosfmt
+    pkgs.trunk
+  ];
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
