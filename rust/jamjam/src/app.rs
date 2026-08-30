6804843125ebe22b508e4b166c38c26048fc4d72
Eva Emmerich <eva@evaemmerich.com>
2026-08-30 17:12:03 +0200

    axum sse counter example functional

--- rust/jamjam/src/app.rs
+++ rust/jamjam/src/app.rs
@@ -1,61 +1,165 @@
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
-    components::{Route, Router, Routes},
+    components::{FlatRoutes, Route, Router, A},
     StaticSegment,
 };
 
+#[cfg(feature = "ssr")]
+use tracing::instrument;
+
+#[derive(Debug, Clone)]
+pub struct GlobalQueue {
+    pub queue: Vec<String>,
+    pub count: usize,
+    pub leptos_options: LeptosOptions,
+}
+
+#[cfg(feature = "ssr")]
+pub mod ssr_imports {
+    pub use broadcaster::BroadcastChannel;
+    pub use std::sync::atomic::{AtomicI32, Ordering};
+    use std::sync::LazyLock;
+
+    pub static COUNT: AtomicI32 = AtomicI32::new(0);
+
+    pub static COUNT_CHANNEL: LazyLock<BroadcastChannel<i32>> =
+        LazyLock::new(BroadcastChannel::<i32>::new);
+}
+
+#[server]
+#[cfg_attr(feature = "ssr", instrument)]
+pub async fn get_server_count() -> Result<i32, ServerFnError> {
+    use ssr_imports::*;
+
+    Ok(COUNT.load(Ordering::Relaxed))
+}
+
+#[server]
+#[cfg_attr(feature = "ssr", instrument)]
+pub async fn adjust_server_count(delta: i32, msg: String) -> Result<i32, ServerFnError> {
+    use ssr_imports::*;
+
+    let new = COUNT.load(Ordering::Relaxed) + delta;
+    COUNT.store(new, Ordering::Relaxed);
+    _ = COUNT_CHANNEL.send(&new).await;
+    println!("message = {:?}", msg);
+    Ok(new)
+}
+
+#[server]
+#[cfg_attr(feature = "ssr", instrument)]
+pub async fn clear_server_count() -> Result<i32, ServerFnError> {
+    use ssr_imports::*;
+
+    COUNT.store(0, Ordering::Relaxed);
+    _ = COUNT_CHANNEL.send(&0).await;
+    Ok(0)
+}
+
+#[cfg(feature = "ssr")]
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
         <!DOCTYPE html>
         <html lang="en">
             <head>
                 <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <AutoReload options=options.clone() />
-                <HydrationScripts options/>
-                <MetaTags/>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <AutoReload options=options.clone()/>
+                <HydrationScripts options=options.clone()/>
             </head>
             <body>
-                <App/>
+                <Counters/>
             </body>
         </html>
     }
 }
 
 #[component]
-pub fn App() -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context();
-
+pub fn Counters() -> impl IntoView {
     view! {
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/jamjam.css"/>
-
-        // sets the document title
-        <Title text="Welcome to Leptos"/>
-
-        // content for this welcome page
         <Router>
+            <header>
+                <h1>"Server-Side Counters"</h1>
+                <p>"Each of these counters stores its data in the same variable on the server."</p>
+                <p>
+                    "The value is shared across connections. Try opening this is another browser tab to see what I mean."
+                </p>
+            </header>
+            <nav>
+                <ul>
+                    <li>
+                        <A href="multi">"Multi-User"</A>
+                    </li>
+                </ul>
+            </nav>
             <main>
-                <Routes fallback=|| "Page not found.".into_view()>
-                    <Route path=StaticSegment("") view=HomePage/>
-                </Routes>
+                <FlatRoutes fallback=|| "Counter Not found.">
+                    <Route path=StaticSegment("multi") view=MultiuserCounter/>
+                </FlatRoutes>
             </main>
         </Router>
     }
 }
 
-/// Renders the home page of your application.
+// This is a kind of "multi-user" counter
+// It relies on a stream of server-sent events (SSE) for the counter's value
+// Whenever another user updates the value, it will update here
+// This is the primitive pattern for live chat, collaborative editing, etc.
 #[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
+pub fn MultiuserCounter() -> impl IntoView {
+    let dec = Action::new(|_: &()| adjust_server_count(-1, "dec dec goose".into()));
+    let inc = Action::new(|_: &()| adjust_server_count(1, "inc inc moose".into()));
+    let clear = Action::new(|_: &()| clear_server_count());
+
+    #[cfg(not(feature = "ssr"))]
+    let multiplayer_value = {
+        use futures::StreamExt;
+        use send_wrapper::SendWrapper;
+
+        let mut source = SendWrapper::new(
+            gloo_net::eventsource::futures::EventSource::new("/api/events")
+                .expect("couldn't connect to SSE stream"),
+        );
+        let ret =
+            ReadSignal::from_stream_unsync(source.subscribe("message").unwrap().map(|value| {
+                match value {
+                    Ok(value) => value.1.data().as_string().expect("expected string value"),
+                    Err(_) => "0".to_string(),
+                }
+            }));
+
+        on_cleanup(move || source.take().close());
+        ret
+    };
+
+    #[cfg(feature = "ssr")]
+    let (multiplayer_value, _) = signal(None::<i32>);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <div>
+            <h2>"Multi-User Counter"</h2>
+            <p>
+                "This one uses server-sent events (SSE) to live-update when other users make changes."
+            </p>
+            <div>
+                <button on:click=move |_| { clear.dispatch(()); }>"Clear"</button>
+                <button on:click=move |_| { dec.dispatch(()); }>"-1"</button>
+                <span>
+                    "Multiplayer Value: " {move || multiplayer_value.get().unwrap_or_default()}
+                </span>
+                <button on:click=move |_| { inc.dispatch(()); }>"+1"</button>
+            </div>
+        </div>
     }
 }
+
+// #[server]
+// pub async fn jam_jam_queue() -> Result<usize, ServerFnError> {
+//     let state = expect_context::<GlobalQueue>();
+//     state.count += 1;
+//     Ok(state.count)
+// }
--- rust/jamjam/src/main.rs
+++ rust/jamjam/src/main.rs
@@ -1,20 +1,45 @@
+mod app;
+
+#[cfg(feature = "ssr")]
+use axum::response::sse::{Event, KeepAlive, Sse};
+#[cfg(feature = "ssr")]
+use futures_util::stream::Stream;
+#[cfg(feature = "ssr")]
+use std::convert::Infallible;
+
+#[cfg(feature = "ssr")]
+async fn counter_events() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    use app::get_server_count;
+    use app::ssr_imports::*;
+    use axum::response::sse::{Event, KeepAlive, Sse};
+    use futures::StreamExt;
+
+    let stream = futures::stream::once(async { get_server_count().await.unwrap_or(0) })
+        .chain(COUNT_CHANNEL.clone())
+        .map(|value| Ok(Event::default().data(format!("event: message\ndata: {value}\n\n"))));
+    Sse::new(stream).keep_alive(KeepAlive::default())
+}
 
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
+    use axum::routing::get;
     use axum::Router;
+    use jamjam::app::*;
     use leptos::logging::log;
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use jamjam::app::*;
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(App);
+    let routes = generate_route_list(Counters);
 
     let app = Router::new()
+        // server sent events cant be done with leptos server functions (yet?)
+        // so we need to register it explicitly
+        .route("/api/events", get(counter_events))
         .leptos_routes(&leptos_options, routes, {
             let leptos_options = leptos_options.clone();
             move || shell(leptos_options.clone())
